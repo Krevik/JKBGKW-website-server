aadc8f040485bceacc879ae34a989dc7bd6b242d
Krzysztof Walczak <kczyku.kw@gmail.com>
2023-02-22 12:04:50 +0100

    Finally working

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const pino = require("express-pino-logger")();
+const fetch = require("node-fetch");
+const cors = require("cors");
+const https = require("https");
+const fs = require("fs");
+const { URLSearchParams } = require("url");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
+app.use(cors());
 
 app.get("/api/greeting", (req, res) => {
 	const name = req.query.name || "World";
@@ -13,20 +19,65 @@ app.get("/api/greeting", (req, res) => {
 	res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
 });
 
-app.get("/api/serverInfo", (req, res) => {
+app.options("/api/serverInfo", cors());
+app.post("/api/serverInfo", cors(), async (req, res) => {
 	res.setHeader("Content-Type", "application/json");
 	res.setHeader("Access-Control-Allow-Origin", "*");
-	fetch(
-		"https://rec.liveserver.pl/api?channel=get_server_info&return_method=json&client_id=26606&pin=635577095f13a5c85545c4e6690d8878&server_id=24044",
+	// @ts-ignore
+	await fetch(
+		"https://rec.liveserver.pl/api?channel=get_server_info&return_method=json",
 		{
 			method: "post",
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+			},
+			body: new URLSearchParams({
+				client_id: "26606",
+				pin: "635577095f13a5c85545c4e6690d8878",
+				server_id: "24044",
+			}),
 		}
-	).then((response) => {
-		console.log("response" + JSON.stringify(response));
-		res.send(response);
+	).then(async (response) => {
+		console.log(response.text);
+		const apiResponseJson = await response.json();
+		res.send(apiResponseJson);
 	});
 });
 
-app.listen(3001, () =>
-	console.log("Express server is running on localhost:3001")
-);
+app.options("/api/steamUserData", (req, res) => {
+	res.setHeader("Content-Type", "application/json");
+});
+
+app.post("/api/steamUserData", async (req, res) => {
+	res.setHeader("Content-Type", "application/json");
+	const fetchURL = `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=F9B6127DDEB6AF27EA0D64F1E5C642A4&steamids=${req.body.userID}`;
+	// @ts-ignore
+	await fetch(fetchURL, {
+		method: "get",
+	})
+		.then(async (response) => {
+			const apiResponseJson = await response.json();
+			res.send(apiResponseJson);
+		})
+		.catch((error) => {
+			console.log("Got error while fetching :( " + error);
+		});
+});
+
+https
+	.createServer(
+		// Provide the private and public key to the server by reading each
+		// file's content with the readFileSync() method.
+		{
+			key: fs.readFileSync("key.pem"),
+			cert: fs.readFileSync("cert.pem"),
+		},
+		app
+	)
+	.listen(3001, () => {
+		console.log("serever is runing at port 3001");
+	});
+
+// app.listen(3001, () =>
+// 	console.log("Express server is running on localhost:3001")
+// );
